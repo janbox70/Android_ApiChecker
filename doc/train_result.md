f58ee5c845ae1a284077971d2f60d01a5e88bce0
xujianbo01 <xujianbo01@baidu.com>
2023-05-08 20:00:33 +0800

    代码优化

--- doc/train_result.md
+++ doc/train_result.md
@@ -28,3 +28,59 @@ Training Classification Model 8: <class 'sklearn.neural_network._multilayer_perc
 Precision 0.976294
 RECALL 0.970849
 ```
+
+
+```
+Total line 501971, Black: 38698
+[0] Training Classification Model BernoulliNB
+    saved to ../output\BernoulliNB.model.pkl
+    Precision = 0.330938,  Recall = 0.770596,  F1 = 0.463026,  cost = 2.760
+[1] Training Classification Model LinearRegression
+    saved to ../output\LinearRegression.model.pkl
+    Precision = 0.959259,  Recall = 0.787833,  F1 = 0.865136,  cost = 6.500
+[2] Training Classification Model RandomForestClassifier
+    saved to ../output\RandomForestClassifier.model.pkl
+    Precision = 0.983226,  Recall = 0.965779,  F1 = 0.974425,  cost = 26.313
+[3] Training Classification Model DecisionTreeClassifier
+    saved to ../output\DecisionTreeClassifier.model.pkl
+    Precision = 0.968077,  Recall = 0.968568,  F1 = 0.968322,  cost = 30.484
+[4] Training Classification Model KNeighborsClassifier
+    saved to ../output\KNeighborsClassifier.model.pkl
+    Precision = 0.973388,  Recall = 0.964259,  F1 = 0.968802,  cost = 642.317
+[5] Training Classification Model MLPClassifier
+    saved to ../output\MLPClassifier.model.pkl
+    Precision = 0.982714,  Recall = 0.965526,  F1 = 0.974044,  cost = 442.604
+[6] Training Classification Model MLPClassifier_2
+    saved to ../output\MLPClassifier_2.model.pkl
+    Precision = 0.978244,  Recall = 0.968821,  F1 = 0.973510,  cost = 1110.077
+[7] Training Classification Model GradientBoostingClassifier
+    saved to ../output\GradientBoostingClassifier.model.pkl
+    Precision = 0.964446,  Recall = 0.873257,  F1 = 0.916589,  cost = 692.927
+[8] Training Classification Model HistGradientBoostingClassifier
+    saved to ../output\HistGradientBoostingClassifier.model.pkl
+    Precision = 0.964761,  Recall = 0.881369,  F1 = 0.921182,  cost = 22.514
+```
+
+```
+Total line 501971, Black: 38698,  features: 242
+[0] Training Classification Model BernoulliNB
+    train = 451773,  test = 50198
+    saved to ../output\BernoulliNB.model.pkl
+    Precision = 0.330938,  Recall = 0.770596,  F1 = 0.463026,  train_cost = 2.226, predict_cost = 0.267
+[1] Training Classification Model LinearRegression
+    train = 451773,  test = 50198
+    saved to ../output\LinearRegression.model.pkl
+    Precision = 0.959259,  Recall = 0.787833,  F1 = 0.865136,  train_cost = 6.884, predict_cost = 0.137
+[2] Training Classification Model RandomForestClassifier
+    train = 451773,  test = 50198
+    saved to ../output\RandomForestClassifier.model.pkl
+    Precision = 0.983234,  Recall = 0.966286,  F1 = 0.974687,  train_cost = 20.753, predict_cost = 0.334
+[3] Training Classification Model DecisionTreeClassifier
+    train = 451773,  test = 50198
+    saved to ../output\DecisionTreeClassifier.model.pkl
+    Precision = 0.968338,  Recall = 0.969075,  F1 = 0.968706,  train_cost = 29.741, predict_cost = 0.191
+[4] Training Classification Model HistGradientBoostingClassifier
+    train = 451773,  test = 50198
+    saved to ../output\HistGradientBoostingClassifier.model.pkl
+    Precision = 0.964761,  Recall = 0.881369,  F1 = 0.921182,  train_cost = 13.506, predict_cost = 0.552
+```
--- src/train_model.py
+++ src/train_model.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 # coding:utf-8
 """
     comments here
@@ -6,10 +6,11 @@
 import os
 from datetime import datetime
 
-from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier, HistGradientBoostingClassifier
 from sklearn.linear_model import LinearRegression
+from sklearn.tree import DecisionTreeClassifier
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import tree, svm, linear_model
+from sklearn import svm
 # from catboost import CatBoostClassifier
 from sklearn.naive_bayes import BernoulliNB
 from sklearn.metrics import accuracy_score, recall_score, precision_score, f1_score
@@ -22,89 +23,123 @@ import pickle as pkl
 
 __author__ = 'apichecker'
 
+classifiers = (
+    "BernoulliNB",
+    "LinearRegression",
+    "RandomForestClassifier",
+    "DecisionTreeClassifier",
+    # "KNeighborsClassifier",
+    # "SVC",            # not suitable for large dataset
+    # "MLPClassifier",
+    # "MLPClassifier_2",
+    # "GradientBoostingClassifier",   # slower than HistGradientBoostingClassifier
+    "HistGradientBoostingClassifier",
+)
 
-def create_classifer(name):
+
+def create_classifier(name):
     if name == "BernoulliNB":
         clf = BernoulliNB()
     elif name == "RandomForestClassifier":
         clf = RandomForestClassifier(n_estimators=15)
     elif name == "DecisionTreeClassifier":
-        clf = tree.DecisionTreeClassifier()
-    elif name == "LinearRegression":
-        clf = LinearRegression()
+        clf = DecisionTreeClassifier()
     elif name == "KNeighborsClassifier":
         clf = KNeighborsClassifier(n_neighbors=5, weights='uniform', algorithm='auto')
     elif name == "SVC":
         clf = svm.SVC(gamma='auto')
     elif name == "MLPClassifier":
-        clf = MLPClassifier(hidden_layer_sizes=(100), solver='adam', alpha=0.0001)
+        clf = MLPClassifier(hidden_layer_sizes=(100,), solver='adam', alpha=0.0001)
     elif name == "MLPClassifier_2":
         clf = MLPClassifier(hidden_layer_sizes=(100, 75, 50, 20), solver='adam', alpha=0.0001)
-    else: # name == "GradientBoostingClassifier":
+    elif name == "GradientBoostingClassifier":
         clf = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=0)
+    elif name == "HistGradientBoostingClassifier":
+        clf = HistGradientBoostingClassifier(learning_rate=0.1, max_depth=3, random_state=0)
+    elif name == "LinearRegression":
+        clf = LinearRegression()
+    else:
+        assert False
 
     return clf
 
 
-if __name__ == "__main__":
+def load_dataset():
     with open("../data/apichecker.pkl", "rb") as pklfile:
         X, y = pkl.load(pklfile)
         y = [int(_) for _ in y]
         total_count = len(X)
         black_count = len(list(filter(lambda _: _ == 1, y)))
-        print("Total line %d, Black: %d" % (total_count, black_count))
-        freq_counter = {}
-        black_freq_counter = {}
-        for x_line, y_line in zip(X, y):
-            for i, feature in enumerate(x_line):
-                if feature == 1:
-                    if i not in freq_counter:
-                        freq_counter[i] = 1
-                        black_freq_counter[i] = 0
-                    else:
-                        freq_counter[i] += 1
+        print("Total line %d, Black: %d,  features: %d" % (total_count, black_count, len(X[0])))
+        # dump_freq(X, y)
+        return X, y
 
-                    if y_line == 1:
-                        black_freq_counter[i] += 1
-        classifiers = ["BernoulliNB",
-                       "RandomForestClassifier",
-                       "DecisionTreeClassifier",
-                       "LinearRegression",
-                       "KNeighborsClassifier",
-                       "SVC",
-                       "MLPClassifier",
-                       "MLPClassifier_2",
-                       "GradientBoostingClassifier"
-                       ]
-        for i, name in enumerate(classifiers):
-            kf = KFold(n_splits=10)
-            round = 1
-            X = np.array(X)
-            y = np.array(y)
-            for train_index, test_index in kf.split(X, y):
-                round += 1
-                X_train, X_test = X[train_index], X[test_index]
-                y_train, y_test = y[train_index], y[test_index]
 
-                clf = create_classifer(name)
-                print("Training Classification Model %d: %s" % (i, name))
-                start = datetime.now()
-                clf.fit(X_train, y_train)
+def dump_freq(X, y):
+    freq_counter = {}
+    black_freq_counter = {}
+    for x_line, y_line in zip(X, y):
+        for i, feature in enumerate(x_line):
+            if feature == 1:
+                if i not in freq_counter:
+                    freq_counter[i] = 1
+                    black_freq_counter[i] = 0
+                else:
+                    freq_counter[i] += 1
 
-                filename = os.path.join("../output", name + ".model.pkl")
-                with open(filename, "wb") as modelfile:
-                    pkl.dump(clf, modelfile)
-                    print("    saved to " + filename)
+                if y_line == 1:
+                    black_freq_counter[i] += 1
 
-                predict = clf.predict(X_test)
-                predict = [1 if p > 0.5 else 0 for p in predict]
-                # print(predict)
+    for i in sorted(freq_counter.keys()):
+        print("[{}]: freq={} black={}".format(i, freq_counter[i], black_freq_counter[i]))
 
-                print("    Precision = %f,  Recall = %f,  F1 = %f,  cost = %.3f" % (
-                    precision_score(y_test, predict),
-                    recall_score(y_test, predict),
-                    f1_score(y_test, predict),
-                    (datetime.now() - start).total_seconds()
-                ))
-                break
+
+def do_train(name, X_train, y_train, X_test, y_test):
+    clf = create_classifier(name)
+    start = datetime.now()
+    clf.fit(X_train, y_train)
+
+    filename = os.path.join("../output", name + ".model.pkl")
+    with open(filename, "wb") as mf:
+        pkl.dump(clf, mf)
+        print("    saved to " + filename)
+
+    train_cost = (datetime.now() - start).total_seconds()
+    start = datetime.now()
+    predict = clf.predict(X_test)
+    predict = [1 if p > 0.5 else 0 for p in predict]
+    # print(predict)
+
+    print("    Precision = %f,  Recall = %f,  F1 = %f,  train_cost = %.3f, predict_cost = %.3f" % (
+        precision_score(y_test, predict),
+        recall_score(y_test, predict),
+        f1_score(y_test, predict),
+        train_cost,
+        (datetime.now() - start).total_seconds()))
+    pass
+
+
+def main():
+    X, y = load_dataset()
+
+    X = np.array(X)
+    y = np.array(y)
+
+    for i, name in enumerate(classifiers):
+        kf = KFold(n_splits=10)
+        round = 1
+        for train_index, test_index in kf.split(X, y):
+            round += 1
+            X_train, X_test = X[train_index], X[test_index]
+            y_train, y_test = y[train_index], y[test_index]
+
+            print("[%d] Training Classification Model %s" % (i, name))
+            print("    train = {},  test = {}".format(len(y_train), len(y_test)))
+            do_train(name, X_train, y_train, X_test, y_test)
+
+            break
         pass
+
+
+if __name__ == "__main__":
+    main()
